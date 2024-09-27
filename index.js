f8121d9e4c5b74516a617b4a4308fe4ac99dfef4
Dioni Alves <dionialves@gmail.com>
2024-09-27 15:30:25 -0300

    Adicionado arquivo api.conf para deixar separado do codigo as informações do servidor.

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-
-
 const Zabbix = require("./lib/Zabbix");
 const Client = require('./lib/Client');
 const fs = require('fs');
 
+const getInformation = () => {
+  try {
+    const data = fs.readFileSync('./api.conf', 'utf-8');
+
+    const lines = data.split('\n');
+    const info = {};
+
+    lines.forEach(lines => {
+
+      if (!lines.startsWith('#')) {
+
+        const [key, value] = lines.split('=').map(item => item.trim());
+
+        if (key && value) {
+          info[key] = value;
+        }
+      }
+    });
+
+    return info;  
+  } 
+  catch (err) {
+    console.error('Erro ao ler o arquivo de configuração:', err);
+    return null;
+  }
+}
+
+const info = getInformation();
+
 const api = new Zabbix({
-    url: 'http://<URL ZABBIX>/zabbix/api_jsonrpc.php',
-    user: '<USERNAME>',
-    password: '<PASSWORD>'
-  })
+  url: info['url'],
+  user: info['user'],
+  password: info['password']
+ }) 
 
 const client = new Client(api)
 
 const main = async () => {
     try {
-        const arrayGroups = await client.getProblems()
+        const arrayGroups = await client.getHostsProblems()
         const data = JSON.stringify(arrayGroups)
 
-        fs.writeFile("./files/example.txt",data , function(err){
+        fs.writeFile("./files/problem_group_host",data , function(err){
             if(err){
                   return console.log('erro')
               }
@@ -28,4 +55,5 @@ const main = async () => {
         console.error(error)
       }
 }
+main()
 setInterval(main, 60000)
